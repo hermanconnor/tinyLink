470f48f344796d69048cbe1ac50db2a09c8285eb
herman <hermanconnor11@gmail.com>
2023-03-26 01:38:51 -0400

    Added cookie-parser and nanoid

---
+++ server/src/models/Url.ts
@@ -0,0 +1,30 @@
+import { Schema, Types, model } from 'mongoose';
+
+interface IUrl {
+  user: Types.ObjectId;
+  urlCode: string;
+  originalLink: string;
+  name?: string;
+  visitCount: number;
+}
+
+const urlSchema = new Schema<IUrl>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+
+    name: {
+      type: String,
+    },
+
+    visitCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export default model('Url', urlSchema);
